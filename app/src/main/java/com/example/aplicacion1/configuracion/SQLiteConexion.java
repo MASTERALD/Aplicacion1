41bf3782346a0313947173a2d979de800288923f
MASTERALD <alexmeraz4567@gmail.com>
2023-02-14 12:47:24 -0600

    Cambio realizado PUSH en Githuv

---
+++ app/src/main/java/com/example/aplicacion1/configuracion/SQLiteConexion.java
@@ -0,0 +1,33 @@
+package com.example.aplicacion1.configuracion;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+
+import com.example.aplicacion1.transacciones.Transacciones;
+
+public class SQLiteConexion  extends SQLiteOpenHelper
+{
+
+    //Constructor de clase con parametros
+    public SQLiteConexion(Context context, String dbname, SQLiteDatabase.CursorFactory factory, int version)
+    {
+        super(context, dbname,factory,version);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+        // Lista de las tablas a crear
+        sqLiteDatabase.execSQL(Transacciones.CreateTBPersonas);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(Transacciones.CreateTBPersonas);
+
+    }
+
+}
